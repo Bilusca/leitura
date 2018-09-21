2c2a52d17c02bba5ed9a37bd73be81141ae62be4
lucasactuar <lucas.vieira@actuar.group>
2018-09-21 17:57:46 -0300

    Api Funcionando

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { getAllCategories } from '../utils/API';
 
 class App extends Component {
+  state = {
+    categories: []
+  }
+
+  componentDidMount() {
+    getAllCategories()
+      .then(res => console.log(res))
+  }
+
   render() {
     return (
       <div>
-       
+        <h1>DEU CERTO</h1>
       </div>
     );
   }
