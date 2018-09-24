aab263d3fe36a61641561ffd8b1524563dca637f
Lucas Assis Vieira <lucasav2005@hotmail.com>
2018-09-23 22:35:38 -0300

    Add coisas

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    Aqui Vai Algo
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </header>
+);
+
+export default Header;
